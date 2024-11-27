0007455a4ab30a82553dd3d7507e2401cdafcaaf
Vladyslav Kucherenko <vlados.soc@gmail.com>
2024-11-27 11:52:55 +0200

    create view for tags and add path to this view

--- task_manager/views.py
+++ task_manager/views.py
@@ -1,10 +1,14 @@
 from django.shortcuts import render
 from django.views import generic
 
-from task_manager.models import Task
+from task_manager.models import Task, Tag
 
 
 class TaskListView(generic.ListView):
     model = Task
-    template_name = "task_list.html"
+    template_name = "task_manager/task_list.html"
     queryset = Task.objects.prefetch_related("tags")
+
+
+class TagListView(generic.ListView):
+    model = Tag
